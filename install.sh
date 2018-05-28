c9b17365ebd47f1d75409c5681efd4a5d66cc128
Yasser Aquino <yaquino@c02nffddg3qd.wvrgroup.internal>
2018-05-28 15:00:01 -0500

    Install script

--- install.sh
+++ install.sh
@@ -5,17 +5,6 @@ systemctl enable docker
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Docker user
-DOCKER_SOCKET=/var/run/docker.sock
-DOCKER_GROUP=docker
-JENKINS_USER=jenkins
-
-if [ -S ${DOCKER_SOCKET} ]; then
-  DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-  sudo groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
-  sudo usermod -aG ${DOCKER_GROUP} ${JENKINS_USER}
-fi
-
 # Installing Java 8 Oracle
 add-apt-repository ppa:webupd8team/java
 apt-get update
@@ -33,6 +22,17 @@ systemctl start jenkins
 systemctl enable jenkins
 
 #Jenkins user
+
+DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GROUP=docker
+JENKINS_USER=jenkins
+
+if [ -S ${DOCKER_SOCKET} ]; then
+  DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+  sudo groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
+  sudo usermod -aG ${DOCKER_GROUP} ${JENKINS_USER}
+fi
+
 adduser jenkins sudo
 sh -c "echo 'jenkins ALL=NOPASSWD: ALL' >> /etc/sudoers"
 
